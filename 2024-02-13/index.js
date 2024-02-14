dc1e623a90400e10138684c545ccb82c79ac2b4b
Sleep-coder <coderaarush69@gmail.com>
2024-02-14 12:22:44 +0530

    last task

--- 2024-02-13/index.js
+++ 2024-02-13/index.js
@@ -3,7 +3,11 @@ const student = {
   name: "Aarush",
   age: 21,
   grade: "A",
-  subjects: ["CSS", "JAVASCRIPT", "HTML"],
+  subjects: [
+    { name: "CSS", grade: 80 },
+    { name: "JAVASCRIPT", grade: 65 },
+    { name: "HTML", grade: 90 },
+  ],
 };
 
 // DISPLAY INFO FUNCTION
@@ -27,25 +31,56 @@ displayInfo1.call(student, "Good Morning ");
 
 // function processSubjects
 
-function processSubjects(callback) {
-  student.subjects.forEach((subject) => callback(subject));
+function processSubjects(arrSub, callback) {
+  arrSub.forEach((subject) => {
+    callback(subject);
+  });
 }
 
-// Example callback function to print each subject
+// Callback function to display each subject
 function printSubject(subject) {
-  console.log(`Subject: ${subject}`);
+  console.log(`Subject: ${subject.name}, Score: ${subject.grade}`);
 }
 
-// printSubject callback
-processSubjects(printSubject);
+// Applying the processSubjects function with the displaySubject callback
+processSubjects(student.subjects, printSubject);
 
 // double grades
 function doubleGrades(student) {
-  // Use map to create a new array with doubled grades
-  const doubledGrades = student.grade.map((grades) => ({
-    grade: student.grade * 2,
-  }));
-  console.log("Original Grades:", this.grade);
-  console.log("Doubled Grades:", doubledGrades);
+  const originalGrades = student.subjects.map((subject) => subject.grade);
+
+  const modifiedGrades = student.subjects.map((subject) => {
+    return {
+      name: subject.name,
+      grade: subject.grade * 2,
+    };
+  });
+
+  console.log("Original Grades:", originalGrades);
+  console.log(
+    "Modified Grades:",
+    modifiedGrades.map((subject) => subject.grade)
+  );
 }
+
 doubleGrades(student);
+
+// passing grade
+function passingSubjects(passingGrade = 70) {
+  // Use filter to find subjects with grades above passingGrade
+  const passingSubjects = student.subjects.filter(
+    (subject) => subject.grade >= passingGrade
+  );
+
+  if (passingSubjects.length > 0) {
+    console.log(`Passing Subjects (grade >= ${passingGrade}):\n`);
+    for (const subject of passingSubjects) {
+      console.log(`- ${subject.name}: ${subject.grade}`);
+    }
+  } else {
+    console.log(`No subjects found with passing grade (>= ${passingGrade})`);
+  }
+}
+
+// Call the function
+passingSubjects();
