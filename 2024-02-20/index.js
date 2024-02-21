3efa1e66e3e21b58886339bc560fd524a728bd38
Sleep-coder <coderaarush69@gmail.com>
2024-02-21 11:15:26 +0530

    first commit

--- 2024-02-20/index.js
+++ 2024-02-20/index.js
@@ -5,19 +5,28 @@ const {
   writeToFile,
 } = require("./fileSystemModule");
 
-const directoryPath = "2024-02-20";
-const newDirectoryName = "2024-02-20/newDirectory";
-const filePath = "2024-02-20/newDirectory/read.txt";
-const newContent = "HI,My name is AARUSH";
+let directoryPath = "2024-02-20";
+let newDirectoryName = "2024-02-20/newDirectory";
+let filePath = "2024-02-20/newDirectory/read.txt";
+let newContent = "HI, My name is AARUSH";
 
 // List files
-listFiles(directoryPath);
-
-// Create directory
-createDirectory(newDirectoryName);
-
-// Read file content
-readFileContent(filePath);
-
-// Write to file
-writeToFile(filePath, newContent);
+listFiles(directoryPath)
+  .then(() => {
+    // Create directory
+    return createDirectory(newDirectoryName);
+  })
+  .then(() => {
+    // Read file content
+    return readFileContent(filePath);
+  })
+  .then(() => {
+    // Write to file
+    return writeToFile(filePath, newContent);
+  })
+  .then(() => {
+    console.log("File updated successfully!");
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
